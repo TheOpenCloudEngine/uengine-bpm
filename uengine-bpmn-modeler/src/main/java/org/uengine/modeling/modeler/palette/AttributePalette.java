0792303eab0948133647068442d6303298acf98a
MisakaMikoto <someanspretty@gmail.com>
2015-06-13 17:08:15 +0900

    잘못된 파일 삭제 후 재 추가

---
+++ uengine-bpmn-modeler/src/main/java/org/uengine/modeling/modeler/palette/AttributePalette.java
@@ -0,0 +1,33 @@
+package org.uengine.modeling.modeler.palette;
+
+import org.uengine.kernel.bpmn.face.ProcessVariablePanel;
+import org.uengine.kernel.bpmn.face.RolePanel;
+import org.uengine.modeling.Palette;
+
+/**
+ * Created by Ryuha on 2015-06-12.
+ */
+public class AttributePalette extends Palette{
+    RolePanel rolePanel = new RolePanel();
+    public RolePanel getRolePanel() {
+        return rolePanel;
+    }
+
+    public void setRolePanel(RolePanel rolePanel) {
+        this.rolePanel = rolePanel;
+    }
+
+    ProcessVariablePanel processVariablePanel = new ProcessVariablePanel();
+    public ProcessVariablePanel getProcessVariablePanel() {
+        return processVariablePanel;
+    }
+
+    public void setProcessVariablePanel(ProcessVariablePanel processVariablePanel) {
+        this.processVariablePanel = processVariablePanel;
+    }
+
+    public AttributePalette() {
+        this.setName("Attributes Setting");
+    }
+
+}

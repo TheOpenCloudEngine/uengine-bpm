82980e9f01fa8358927f0a11b69b04b1bd6b7ffa
jinyoung jang <jyjang@uengine.org>
2015-08-26 10:57:16 +0900

    EditorPanel must be scoped as 'prototype' because it should be instantiated per every thread.

--- uengine-social-bpm-portal/src/main/java/org/uengine/processadmin/ProcessAdminEditorPanel.java
+++ uengine-social-bpm-portal/src/main/java/org/uengine/processadmin/ProcessAdminEditorPanel.java
@@ -2,11 +2,13 @@ package org.uengine.processadmin;
 
 import org.metaworks.annotation.AutowiredFromClient;
 import org.metaworks.annotation.ServiceMethod;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.uengine.codi.mw3.model.Session;
 import org.uengine.modeling.resource.EditorPanel;
 
 @Component
+@Scope("prototype")
 public class ProcessAdminEditorPanel extends EditorPanel{
 
 	@ServiceMethod(keyBinding="Ctrl+S", callByContent = true)

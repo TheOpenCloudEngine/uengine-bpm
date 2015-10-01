2f36e8617867aae9ad7e3664b33f082fcd8d538d
jinyoung jang <jyjang@uengine.org>
2015-10-01 10:06:51 +0900

    RoleMapping - User Mapper interfaces

---
+++ uengine-social-bpm-portal/src/main/java/org/uengine/modeling/resource/resources/JavaClassResource.java
@@ -0,0 +1,7 @@
+package org.uengine.modeling.resource.resources;
+
+/**
+ * Created by jangjinyoung on 15. 9. 27..
+ */
+public class JavaClassResource {
+}
---
+++ uengine-social-bpm-portal/src/main/java/org/uengine/social/RoleUser.java
@@ -0,0 +1,7 @@
+package org.uengine.social;
+
+/**
+ * Created by jangjinyoung on 15. 9. 27..
+ */
+public class RoleUser {
+}

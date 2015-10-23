cfe9c823b16f19832aa59f3686b611cf27bd7352
horklim <lnhao06@gmail.com>
2015-10-23 17:36:25 +0900

    [uengine5.0] Process Modeler에서 팝업 방식으로 Role를 추가

--- uengine-core/src/main/java/org/uengine/kernel/bpmn/face/RolePanel.java
+++ uengine-core/src/main/java/org/uengine/kernel/bpmn/face/RolePanel.java
@@ -1,10 +1,15 @@
 package org.uengine.kernel.bpmn.face;
 
+import org.metaworks.MetaworksContext;
 import org.metaworks.annotation.Face;
+import org.metaworks.annotation.ServiceMethod;
+import org.metaworks.widget.ModalWindow;
 import org.uengine.kernel.Role;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.metaworks.dwr.MetaworksRemoteService.wrapReturn;
 
 public class RolePanel{
     List<Role> roleList = new ArrayList<Role>();
@@ -16,6 +21,4 @@ public class RolePanel{
         public void setRoleList(List<Role> roleList) {
             this.roleList = roleList;
         }
-
-
 }

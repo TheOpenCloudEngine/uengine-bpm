e569476a0dd292f9c467d55dc8184af15f91b0c4
MisakaMikoto <someanspretty@gmail.com>
2016-06-22 17:13:39 +0900

    add find subprocess child in export msproject

--- bpmn-model/src/main/java/org/uengine/processpublisher/microsoft/exporter/ProcessDefinitionAdapter.java
+++ bpmn-model/src/main/java/org/uengine/processpublisher/microsoft/exporter/ProcessDefinitionAdapter.java
@@ -1,10 +1,8 @@
 package org.uengine.processpublisher.microsoft.exporter;
 
 import net.sf.mpxj.*;
-import org.uengine.kernel.Activity;
-import org.uengine.kernel.HumanActivity;
-import org.uengine.kernel.ProcessDefinition;
-import org.uengine.kernel.Role;
+import org.uengine.kernel.*;
+import org.uengine.kernel.bpmn.SubProcess;
 import org.uengine.processpublisher.Adapter;
 import java.util.Hashtable;
 
@@ -36,28 +34,58 @@ public class ProcessDefinitionAdapter implements Adapter<ProcessDefinition, Proj
         // find Activity
         if (src.getChildActivities() != null && src.getChildActivities().size() > 0) {
             for (Activity activity : src.getChildActivities()) {
+                // if subProcess
+                if(activity instanceof SubProcess) {
+                    subProcessConvert((SubProcess) activity, projectFile);
+                }
+
                 // if HumanActivity
                 if (activity instanceof HumanActivity) {
-                    HumanActivity humanActivity = (HumanActivity) activity;
-
-                    // task add to project
-                    Task task = projectFile.addTask();
-                    this.setTaskUniqueID(task, humanActivity);
-                    this.setTaskName(task, humanActivity);
-                    this.setTaskDuration(task, humanActivity);
-                    this.setTaskPredecessor(task, humanActivity, projectFile);
+                    setTask((HumanActivity) activity, projectFile);
 
                 }
             }
         }
 
+        // find role
         if(src.getRoles() != null && src.getRoles().length > 0) {
-            for(Role role : src.getRoles()) {
-                Resource resource = projectFile.addResource();
-                this.setResourceName(resource, role);
-            }
+            setResource(src, projectFile);
         }
         return projectFile;
+    }
+
+    private void subProcessConvert(SubProcess subProcess, ProjectFile projectFile) {
+        // find Activity
+        if (subProcess.getChildActivities() != null && subProcess.getChildActivities().size() > 0) {
+            for (Activity activity : subProcess.getChildActivities()) {
+                // if subProcess
+                if(activity instanceof SubProcess) {
+                    subProcessConvert((SubProcess) activity, projectFile);
+                }
+
+                // if HumanActivity
+                if (activity instanceof HumanActivity) {
+                    setTask((HumanActivity) activity, projectFile);
+
+                }
+            }
+        }
+    }
+
+    private void setTask(HumanActivity humanActivity, ProjectFile projectFile) {
+        // task add to project
+        Task task = projectFile.addTask();
+        this.setTaskUniqueID(task, humanActivity);
+        this.setTaskName(task, humanActivity);
+        this.setTaskDuration(task, humanActivity);
+        this.setTaskPredecessor(task, humanActivity, projectFile);
+    }
+
+    private void setResource(ProcessDefinition src, ProjectFile projectFile) {
+        for(Role role : src.getRoles()) {
+            Resource resource = projectFile.addResource();
+            this.setResourceName(resource, role);
+        }
     }
 
     private void setTaskUniqueID(Task currentTask, HumanActivity currentHumanActivity) {
@@ -66,7 +94,7 @@ public class ProcessDefinitionAdapter implements Adapter<ProcessDefinition, Proj
     }
 
     private void setTaskName(Task currentTask, HumanActivity currentHumanActivity) {
-        // if humanActivty mapped role, task name = task name + role name
+        // if humanActivity mapped role, task name = task name + role name
         currentTask.setName(currentHumanActivity.getName());
     }
 
@@ -76,7 +104,7 @@ public class ProcessDefinitionAdapter implements Adapter<ProcessDefinition, Proj
     }
 
     private void setTaskPredecessor(Task currentTask, HumanActivity currentHumanActivity, ProjectFile projectFile) {
-        if(currentHumanActivity.getIncomingSequenceFlows().get(0).getSourceElement() instanceof HumanActivity) {
+        if(currentHumanActivity.getIncomingSequenceFlows().size() > 0 && currentHumanActivity.getIncomingSequenceFlows().get(0).getSourceElement() instanceof HumanActivity) {
             HumanActivity incomingHumanActivity = (HumanActivity) currentHumanActivity.getIncomingSequenceFlows().get(0).getSourceElement();
             // humanActivity's tracingtag == task's uniqueID
             int taskUniqueID = Integer.parseInt(incomingHumanActivity.getTracingTag());

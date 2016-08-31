688bdd229da10c078299e933931e0288ff56bc09
basquiat <funnyjazz@naver.com>
2016-08-31 09:53:31 +0900

    프로세스 리스트 우클릭 시뮬레이터 처리

--- uengine-social-bpm-portal/src/main/java/org/uengine/modeling/resource/resources/ProcessResource.java
+++ uengine-social-bpm-portal/src/main/java/org/uengine/modeling/resource/resources/ProcessResource.java
@@ -3,7 +3,10 @@ package org.uengine.modeling.resource.resources;
 import org.metaworks.EventContext;
 import org.metaworks.ServiceMethodContext;
 import org.metaworks.annotation.*;
+import org.metaworks.dao.TransactionContext;
 import org.metaworks.dwr.MetaworksRemoteService;
+import org.metaworks.widget.Label;
+import org.metaworks.widget.ModalWindow;
 import org.metaworks.widget.ToBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -12,8 +15,13 @@ import org.uengine.codi.CodiProcessDefinitionFactory;
 import org.uengine.codi.mw3.model.Session;
 import org.uengine.kernel.ProcessDefinition;
 import org.uengine.kernel.ProcessDefinitionFactory;
-import org.uengine.modeling.resource.DefaultResource;
-import org.uengine.modeling.resource.ResourceControlDelegate;
+import org.uengine.modeling.HasThumbnail;
+import org.uengine.modeling.Modeler;
+import org.uengine.modeling.resource.*;
+import org.uengine.modeling.resource.editor.ProcessEditor;
+
+import static org.metaworks.dwr.MetaworksRemoteService.autowire;
+import static org.metaworks.dwr.MetaworksRemoteService.wrapReturn;
 
 /**
  * Created by jangjinyoung on 15. 7. 12..
@@ -24,6 +32,14 @@ public class ProcessResource extends DefaultResource {
 
     @AutowiredFromClient
     public Session session;
+
+    IEditor editor;
+        public IEditor getEditor() {
+            return editor;
+        }
+        public void setEditor(IEditor editor) {
+        this.editor = editor;
+    }
 
     @Override
     public void save(Object editingObject) throws Exception {
@@ -40,6 +56,44 @@ public class ProcessResource extends DefaultResource {
         MetaworksRemoteService.wrapReturn(new ToBlank("resource-editor.html?resourcePath=" + getPath() + "&accessToken=" + session.getEmployee().getEmail()));
     }
 
+    @ServiceMethod(callByContent = true, target = ServiceMethod.TARGET_POPUP, inContextMenu = true)
+    public void simulate() throws Exception {
+
+        try {
+
+            ModalWindow runner = new ModalWindow();
+            runner.setWidth(1000);
+            runner.setTitle("Simulation");
+            runner.setPanel(new ProcessEditor().simulator(this));
+            MetaworksRemoteService.wrapReturn(runner);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        /*
+        TransactionContext.getThreadLocalInstance().setSharedContext("isDevelopmentTime", true);
+        //save();
+        if(getEditor() instanceof Simulatable) {
+            ModalWindow runner = new ModalWindow();
+
+            runner.setWidth(1000);
+            runner.setTitle("Simulation");
+
+            IResource resource = new DefaultResource();
+            resource.setPath(getPath());
+
+            runner.setPanel(((Simulatable) getEditor()).simulator(resource));
+
+            MetaworksRemoteService.wrapReturn(runner);
+        }else {
+            MetaworksRemoteService.wrapReturn(new ModalWindow(new Label("This resource is not supporting simulation")));
+        }
+        */
+    }
 
     @Autowired
     public ProcessDefinitionFactory definitionFactory;

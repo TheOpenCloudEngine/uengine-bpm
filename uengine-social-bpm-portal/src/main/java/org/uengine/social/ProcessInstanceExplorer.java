f60ab040feee6bbc99e6f39b385b7d4415c87c22
admin <admin>
2016-09-20 16:11:31 +0900

    no message

---
+++ uengine-social-bpm-portal/src/main/java/org/uengine/social/ProcessInstanceExplorer.java
@@ -0,0 +1,7 @@
+package org.uengine.social;
+
+/**
+ * Created by kimsh on 2016. 9. 19..
+ */
+public class ProcessInstanceExplorer {
+}
---
+++ uengine-social-bpm-portal/src/main/java/org/uengine/social/SocialBPMInitActivityFilter.java
@@ -0,0 +1,7 @@
+package org.uengine.social;
+
+/**
+ * Created by kimsh on 2016. 9. 17..
+ */
+public class SocialBPMInitActivityFilter {
+}

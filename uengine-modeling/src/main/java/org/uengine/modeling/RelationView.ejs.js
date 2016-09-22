137678d488071681cc24763576d308f1c2812e4b
basquiat <funnyjazz@naver.com>
2016-09-22 09:37:19 +0900

    버블링 방지

--- uengine-modeling/src/main/java/org/uengine/modeling/RelationView.ejs.js
+++ uengine-modeling/src/main/java/org/uengine/modeling/RelationView.ejs.js
@@ -203,6 +203,19 @@ org_uengine_modeling_RelationView.prototype = {
     },
 
     bind: function (name) {
+
+        try{
+
+            var events = $(this.element).data("events");
+
+            for(var i in events){
+                var event = events[i];
+                if(event.namespace == this.objectId)
+                    return; //already existing event
+            }
+
+        }catch(e){}
+
         $(this.element).bind(name + '.' + this.objectId, {objectId: this.objectId}, function (event, ui) {
             $('#' + mw3._getObjectDivId(event.data.objectId)).trigger(event.type);
         });

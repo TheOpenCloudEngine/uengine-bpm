8850ce461a190c4cb957f544072007a8857e0dc9
basquiat <funnyjazz@naver.com>
2017-07-04 15:01:00 +0900

    index

---
+++ bpmn-model/src/main/java/org/uengine/processpublisher/Index.java
@@ -0,0 +1,10 @@
+package org.uengine.processpublisher;
+
+/**
+ * @author Jinyoung Jang
+ */
+
+public class Index {
+    public static ThreadLocal<Integer> indexX = new ThreadLocal<Integer>();
+    public static ThreadLocal<Integer> indexY = new ThreadLocal<Integer>();
+}

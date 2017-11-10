a49a184040bdb60c06c23496f42b5013f8fd2bd2
jinyoung jang <jyjang@uengine.org>
2017-11-10 18:56:01 +0900

    no message

--- uengine-migrator/src/main/java/org/uengine/migrator/UEngine3Converter.java
+++ uengine-migrator/src/main/java/org/uengine/migrator/UEngine3Converter.java
@@ -1,13 +1,13 @@
 package org.uengine.migrator;
 
+import org.uengine.components.serializers.ActivityRepositoryConverter;
 import org.uengine.kernel.*;
 import org.uengine.modeling.resource.Serializer;
 import org.uengine.processpublisher.uengine3.importer.ProcessDefinitionAdapter;
 import org.uengine.util.ActivityFor;
+import org.uengine.util.UEngineUtil;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.Hashtable;
 
 /**
@@ -17,7 +17,24 @@ public class UEngine3Converter {
 
     public static void main(String... args) throws Exception {
 
-        ProcessDefinition processDefinition3 = (ProcessDefinition) Serializer.deserialize(new FileInputStream(args[0]));
+        if(args.length == 0) args = new String[]{"example.3upd"};
+
+//        System.out.println(new File(".").getAbsoluteFile().getPath());
+
+        //Serializer.xstream.registerConverter(new ActivityRepositoryConverter());
+
+        FileInputStream inputFileStream = new FileInputStream(args[0]);
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+
+        UEngineUtil.copyStream(inputFileStream, bao);
+
+        String inputString = bao.toString();
+
+        inputString = inputString.replace("org.uengine.kernel.ActivityRepository", "java.util.ArrayList");
+        inputString = inputString.replace("kitech.apr.activity.KitechHumanActivity", "org.uengine.kernel.HumanActivity");
+
+
+        ProcessDefinition processDefinition3 = (ProcessDefinition) Serializer.deserialize(inputString);
         ProcessDefinition processDefinition5;
 
         ProcessDefinitionAdapter processDefinitionAdapter = new ProcessDefinitionAdapter();
